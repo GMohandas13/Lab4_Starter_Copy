9222f28ae85c4a0d3e96a3148f100cc44d182c30
gautammdas13 <gmohandas2@student.ohlone.edu>
2025-04-29 17:45:49 -0700

    Adding bug fixes for last 2 failed tests

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,11 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    //adding type check
+    if (typeof num1 != "number" || typeof num2 != "number" || typeof add != "boolean"){
+        return false;
+    }
+
     if (add) {
         //const result = 0; const makes it so that the variable cannot be reassigned a different value
         let result = 0;
@@ -26,6 +31,11 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    //adding type checking
+    if (typeof discount != "number" || Array.isArray(prices) == false){
+        return false;
+    }
+
     const discounted = []
     const length = prices.length;
 
